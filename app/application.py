be6e9f2b928ecd6b3ddf4518727bcd7a239765ff
Eric Chu <echu@leviathan.media.mit.edu>
2020-02-01 17:13:12 -0500

    [App] Import from config, point towards symlinked segs dir in static
    Test plan:
    1. Set symlink inside app/static/:
    ln -s ../../data data
    
    2. Update index.html's img_fp and js_fp to be url_for('static', img_fp), i.e.
    static/data/quickdraw/segmentations/...
    
    3. Run from root (we now import config)
    PYTHONPATH=. python app/application.py
    
    4. Visit <lev public hostname>:8080/instruction_tree
    Visit <lev public hostname>:8080/instruction_tree/<category>

--- app/application.py
+++ app/application.py
@@ -12,7 +12,7 @@ Currently implemented:
     - /instruction_tree
 
 Usage:
-    python application.py
+    PYTHONPATH=. python app/application.py
 """
 
 import os
@@ -20,16 +20,15 @@ import random
 
 from flask import Flask, render_template
 
+from config import BEST_SEG_PROGRESSION_PAIRS_PATH, BEST_SEG_NDJSON_PATH
+
 application = Flask(__name__,
                     static_folder='static',
                     template_folder='templates')
 
-# SEG_DIR = 'static/segmentations/greedy_parsing/progressionpair/test/'
-SEG_DIR = 'static/segmentations/greedy_parsing/progressionpair/instruction_to_strokes/dec19/test/'
-
-def load_seg_trees():
-    fns = sorted(os.listdir(SEG_DIR))
-    fps = [os.path.join(SEG_DIR, fn) for fn in fns]
+def load_seg_trees(seg_dir):
+    fns = sorted(os.listdir(seg_dir))
+    fps = [os.path.join(seg_dir, fn) for fn in fns]
     segs = []
     for i in range(0, len(fps), 3):
         seg = (fps[i], fps[i+2])  # .jpg, .json, _treant.js
@@ -39,24 +38,25 @@ def load_seg_trees():
 @application.route('/instruction_tree', methods=['GET'])
 def instruction_tree():
     seg = random.choice(segs)
-    img_fp = seg[0]
-    js_fp = seg[1]
+    img_fp, js_fp = seg
+    category = os.path.basename(img_fp).split('_')[0]
 
     return render_template('index.html',
-                            img_fp=img_fp, js_fp=js_fp)
+                            category=category.title(),
+                            img_fp=img_fp,
+                            js_fp=js_fp)
 
-@application.route('/instruction_tree_<category>', methods=['GET'])
-# @application.route('/instruction_tree/<category>', methods=['GET'])
+@application.route('/instruction_tree/<category>', methods=['GET'])
 def instruction_tree_category(category):
-    cur_segs = [seg for seg in segs if (category in seg[0]) ]
+    cat_segs = [seg for seg in segs if (category in seg[0]) ]
+    seg = random.choice(cat_segs)
+    img_fp, js_fp = seg
 
-    seg = random.choice(cur_segs)
-    img_fp = seg[0]
-    js_fp = seg[1]
-
-    return render_template('index.html',
+    return render_template('index.html', category=category.title(),
                             img_fp=img_fp, js_fp=js_fp)
 
 if __name__ == '__main__':
-    segs = load_seg_trees()
-    application.run(host='0.0.0.0', port=8080, debug=True)
+    seg_dir = BEST_SEG_PROGRESSION_PAIRS_PATH / 'test'
+    print(seg_dir)
+    segs = load_seg_trees(seg_dir)
+    application.run(host='0.0.0.0', port=8080, debug=False)
