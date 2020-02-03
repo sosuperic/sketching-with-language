61178e416eafc4c8083092cfe5e29bfed1056ef0
Eric Chu <echu@leviathan.media.mit.edu>
2020-02-03 01:45:40 -0500

    [App] Show two instruction trees for each image
    Motivation: testing new ways to generate instruction trees,
    want to qualitatively assess the differences
    
    Test plan:
    1. gunicorn -w=2 -b=0.0.0.0:8080 --chdir=app wsgi:application --preload
    2. gunicorn -w=2 -b=0.0.0.0:8080 --chdir=app wsgi:application --preload --daemon
    
    TODO: the above involves killing gunicorn and restarting it every time
    to test... not the proper way to do it. I should be able to do the
    normal PYTHONPATH=. python app/application.py, but there is a path error
    with static now since I refactored to allow gunicorn to work.

--- app/application.py
+++ app/application.py
@@ -33,39 +33,57 @@ application = Flask(__name__,
 application.url_map.strict_slashes = False
 
 def load_seg_trees(seg_dir):
-    fns = sorted(os.listdir(STATIC_FOLDER / seg_dir))  # had to add STATIC_FOLDER for gunicorn...
+    """
+    Args:
+        seg_dir (str): [description]
+
+    Returns:
+        dict: keys are (category, id), values are (img_fp, treant_js_fp)
+    """
+    fns = sorted(os.listdir(os.path.join(STATIC_FOLDER, seg_dir)))  # had to add STATIC_FOLDER for gunicorn...
     fps = [os.path.join(seg_dir, fn) for fn in fns]
-    segs = []
+
+    segs = {}
     for i in range(0, len(fps), 3):
-        seg = (fps[i], fps[i+2])  # .jpg, .json, _treant.js
-        segs.append(seg)
+        img_fp, seg_fp, treant_fp = fps[i], fps[i+1], fps[i+2]  # .jpg, .json, _treant.js
+        img_fn = os.path.basename(img_fp)
+        category, id = img_fn.split('.')[0].split('_')
+        segs[(category, id)] = (img_fp, treant_fp)
     return segs
 
-seg_dir = BEST_SEG_PROGRESSION_PAIRS_PATH / 'train'
-segs = load_seg_trees(seg_dir)   # by doing gunicorn --preload, loads before forking
+# by doing gunicorn --preload, loads before forking
+seg_dir1 = BEST_SEG_PROGRESSION_PAIRS_PATH / 'train'
+seg_dir2 = 'data/quickdraw/segmentations/greedy_parsing/progressionpair/Feb01_2020/strokes_to_instruction/train'
+segs1 = load_seg_trees(seg_dir1)
+segs2 = load_seg_trees(seg_dir2)
 
 #
 # Routes
 #
 @application.route('/instruction_tree', methods=['GET'])
 def instruction_tree():
-    seg = random.choice(segs)
-    img_fp, js_fp = seg
-    category = os.path.basename(img_fp).split('_')[0]
+    key = random.choice(list(segs1.keys()))
+    category, id = key
+
+    img_fp, js_fp1 = segs1[key]
+    _, js_fp2 = segs2[key]
 
     return render_template('index.html',
                             category=category.title(),
-                            img_fp=img_fp,
-                            js_fp=js_fp)
+                            img_fp=img_fp, js_fp1=js_fp1, js_fp2=js_fp2)
 
 @application.route('/instruction_tree/<category>', methods=['GET'])
 def instruction_tree_category(category):
-    cat_segs = [seg for seg in segs if (category in seg[0]) ]
-    seg = random.choice(cat_segs)
-    img_fp, js_fp = seg
+    keys_for_category = [key for key in segs1.keys() if (category == key[0])]
+    key = random.choice(keys_for_category)
+    category, id = key
 
-    return render_template('index.html', category=category.title(),
-                            img_fp=img_fp, js_fp=js_fp)
+    img_fp, js_fp1 = segs1[key]
+    _, js_fp2 = segs2[key]
+
+    return render_template('index.html',
+                            category=category.title(),
+                            img_fp=img_fp, js_fp1=js_fp1, js_fp2=js_fp2)
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=8080, debug=False)
